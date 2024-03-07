d0fed6170e6fcca17c18e54b3458553da34d8d8e
gibranmenezes <gibranquimica@gmail.com>
2024-03-07 08:36:38 -0300

    structuring repository

---
+++ authomated-tests-with-java/src/test/java/io/github/gmenezes/FooBarTest.java
@@ -0,0 +1,11 @@
+package io.github.gmenezes;
+
+import org.junit.jupiter.api.Test;
+
+public class FooBarTest {
+
+    @Test
+    void test(){
+        System.out.println("Hello, JUNIT 5");
+    }
+}
